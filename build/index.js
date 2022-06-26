02ce7bd4ef8a5e19d6f5c9536b44ee6fc65cd768
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-26 11:17:49 +0300

    add: duplex stream, capture screen

--- build/index.js
+++ build/index.js
@@ -1,17 +1,31 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { httpServer } from './http_server/index.js';
 import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
 import * as mouse from './modules/mouseActions.js';
 import * as draw from './modules/drawActions.js';
+import { captureScreen } from './modules/captureScreen.js';
 const HTTP_PORT = 3000;
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 const wss = new WebSocketServer({ port: 8080 });
 let timerMouse;
 wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
-        const [action, command, arg1, arg2] = splitMessage(data);
+    const duplex = createWebSocketStream(ws, {
+        encoding: 'utf8',
+        decodeStrings: false,
+    });
+    duplex.on('data', (chunk) => __awaiter(this, void 0, void 0, function* () {
+        console.log('received: %s', chunk);
+        const [action, command, arg1, arg2] = splitMessage(chunk);
         const mousePoints = robot.getMousePos();
         clearInterval(timerMouse);
         switch (true) {
@@ -25,7 +39,7 @@ wss.on('connection', function connection(ws) {
                 break;
             case action === 'mouse':
                 if (command === 'position') {
-                    ws.send(mouse.position());
+                    duplex.write(mouse.position());
                     break;
                 }
                 if (command === 'up')
@@ -36,13 +50,16 @@ wss.on('connection', function connection(ws) {
                     mouse.left(arg1);
                 if (command === 'right')
                     mouse.right(arg1);
-                ws.send(`mouse_${command}`);
+                duplex.write(`mouse_${command}`);
+                break;
+            case action === 'prnt' && command === 'scrn':
+                const buffer = yield captureScreen();
+                duplex.write(`${buffer}`);
                 break;
             default:
                 break;
         }
-    });
-    ws.send('something');
+    }));
 });
 const splitMessage = (data) => {
     const messageArr = data.toString().split(' ');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // import Jimp from 'jimp';
 import { httpServer } from './http_server/index.js';
 import robot from 'robotjs';
-import WebSocket, { WebSocketServer } from 'ws';
+// import WebSocket, { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer, createWebSocketStream } from 'ws';
 import * as mouse from './modules/mouseActions.js';
 import * as draw from './modules/drawActions.js';
+import { captureScreen } from './modules/captureScreen.js';
 
 const HTTP_PORT = 3000;
 
@@ -11,12 +13,20 @@ console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
 const wss = new WebSocketServer({ port: 8080 });
+// const wss = new WebSocket({ port: 8080 });
+
 let timerMouse;
 
 wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
-        const [action, command, arg1, arg2] = splitMessage(data);
+    const duplex = createWebSocketStream(ws, {
+        encoding: 'utf8',
+        decodeStrings: false,
+    });
+
+    duplex.on('data', async (chunk: Buffer) => {
+        console.log('received: %s', chunk);
+
+        const [action, command, arg1, arg2] = splitMessage(chunk);
         const mousePoints = robot.getMousePos();
         clearInterval(timerMouse);
         switch (true) {
@@ -27,22 +37,24 @@ wss.on('connection', function connection(ws) {
                 break;
             case action === 'mouse':
                 if (command === 'position') {
-                    ws.send(mouse.position());
+                    duplex.write(mouse.position());
                     break;
                 }
                 if (command === 'up') mouse.up(arg1);
                 if (command === 'down') mouse.down(arg1);
                 if (command === 'left') mouse.left(arg1);
                 if (command === 'right') mouse.right(arg1);
-                ws.send(`mouse_${command}`);
+                duplex.write(`mouse_${command}`);
+                break;
+            case action === 'prnt' && command === 'scrn':
+                const buffer = await captureScreen();
+                duplex.write(`${buffer}`);
                 break;
 
             default:
                 break;
         }
     });
-
-    ws.send('something');
 });
 
 const splitMessage = (data) => {
